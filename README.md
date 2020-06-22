dd57b9a32d595f44c4f2364386d6439f91e9ac74
Jim Sellers <jim.sellers@canada.ca>
2020-06-22 16:54:56 -0400

    putting in a command line parser

--- README.md
+++ README.md
@@ -3,4 +3,7 @@ Tests access to a url using http client 4.x
 
 ## Example usage
 Without a proxy
-`java -jar url-tester-0.0.1-SNAPSHOT-jar-with-dependencies.jar www.google.ca`
+`java -jar target/url-tester-0.0.1-SNAPSHOT-jar-with-dependencies.jar -url www.google.ca`
+
+When running behind a proxy
+`java -jar target/url-tester-0.0.1-SNAPSHOT-jar-with-dependencies.jar -proxyHost=myProxyServer -proxyPort=80 www.tsn.ca`
--- src/main/java/com/github/sellers/urltester/UrlTesterMain.java
+++ src/main/java/com/github/sellers/urltester/UrlTesterMain.java
@@ -1,5 +1,12 @@
 package com.github.sellers.urltester;
 
+import org.apache.commons.cli.CommandLine;
+import org.apache.commons.cli.CommandLineParser;
+import org.apache.commons.cli.DefaultParser;
+import org.apache.commons.cli.HelpFormatter;
+import org.apache.commons.cli.Option;
+import org.apache.commons.cli.Options;
+import org.apache.commons.cli.ParseException;
 import org.apache.http.client.methods.CloseableHttpResponse;
 import org.apache.http.client.methods.HttpGet;
 import org.apache.http.impl.client.CloseableHttpClient;
@@ -9,14 +16,46 @@ public class UrlTesterMain {
 
     public static void main(String[] args) {
 
-        if (0 == args.length) {
-            System.out.println("Example usage:");
-            System.out.println("java -jar url-tester-0.0.1-SNAPSHOT-jar-with-dependencies.jar www.google.ca");
+        Options options = new Options();
+        Option urlsOption = new Option("url", null, true, "The Urls to try to contact over https");
+        urlsOption.setArgs(Option.UNLIMITED_VALUES);
+        options.addOption(urlsOption);
 
-        } else {
-            for (String url : args) {
-                checkUrl(url);
+        Option proxyHostOption = new Option("proxyHost", null, true, "The proxy host");
+        proxyHostOption.setOptionalArg(true);
+        options.addOption(proxyHostOption);
+        Option proxyPortOption = new Option("proxyPort", null, true, "The proxy port");
+        proxyPortOption.setOptionalArg(true);
+        options.addOption(proxyPortOption);
+
+        CommandLineParser parser = new DefaultParser();
+        HelpFormatter formatter = new HelpFormatter();
+        CommandLine cmd;
+
+        try {
+            cmd = parser.parse(options, args);
+
+            if (null != cmd.getOptionValue("proxyHost")) {
+                System.setProperty("https.proxyHost", cmd.getOptionValue("proxyHost"));
+                System.setProperty("java.net.useSystemProxies", "true");
             }
+            if (null != cmd.getOptionValue("proxyPort")) {
+                System.setProperty("https.proxyPort", cmd.getOptionValue("proxyPort"));
+            }
+
+            if (null != cmd.getOptionValues("url")) {
+                for (String url : cmd.getOptionValues("url")) {
+                    checkUrl(url);
+                }
+            } else {
+                formatter.printHelp("url-tester", options);
+            }
+
+        } catch (ParseException e) {
+            System.out.println(e.getMessage());
+            formatter.printHelp("url-tester", options);
+
+            System.exit(1);
         }
     }
 
@@ -33,6 +72,7 @@ public class UrlTesterMain {
                 method.releaseConnection();
             }
         } catch (Exception e) {
+            System.out.println(String.format("%s threw an exception of %s", urlAddress, e.getMessage()));
             e.printStackTrace();
         }
     }
